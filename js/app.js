08198ba477c16bfffb6a59cfe506f5b289372acc
phsinghkaeratech <phsinghkaeratech@gmail.com>
2022-07-27 12:36:53 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToscroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            },
+            {
+                breakpoint:768,
+                settings:{
+                    slidesToShow:1,
+                }
+            }
+        ]
+    });
+
+    $('.nav-trigger').click(function(){
+        // alert('vfvjfvndfjsvn');
+        $('.site-content-wrapper').toggleClass('scaled');
+    })
+
+});
+
